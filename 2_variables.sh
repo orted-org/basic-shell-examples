abee7e72bd821323906cc1c9fd64b587e205560f
himanshu-sah <itsmehs07@gmail.com>
2021-09-13 21:42:55 +0545

    variables

---
+++ 2_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VAR1=10
+VAR2=20
+echo $VAR1
+echo $VAR2
+echo "Variable 1 is ${VAR1}"
+echo "Variable 2 is ${VAR2}"
+echo 'This will just print as string itself ${VAR1}'
+
+#Changing variable value
+VAR2=100
+echo "Variable 2 is updated and is ${VAR2}"
