826701bf94dfb4a167b0725e65f37c28a2ff079a
himanshu-sah <itsmehs07@gmail.com>
2021-09-13 22:14:12 +0545

    while loop

--- 7_loops.sh
+++ 7_loops.sh
@@ -5,3 +5,13 @@ for i in {1..10}; do
     SUM=$((SUM + i))
 done
 echo "The sum of first 10 natural numbers is ${SUM}"
+
+for cars in Ferrari Ford Tesla; do
+    echo "${cars}"
+done
+
+i=0
+while [[ i -ne 10 ]]; do
+    echo "While loop iteration count ${i}"
+    ((i++))
+done
